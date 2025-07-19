70ac820fe53d4f271ca09ed6e14b480593a630a6
27pavann <stud.2201201098@kpgu.ac.in>
2025-07-19 17:10:13 +0530

    added

--- models/productModel.js
+++ models/productModel.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: Array, required: true },
-    category: { type: String, required: true },
-    subCategory: { type: String, required: true },
-    sizes: { type: Array, required: true },
-    bestseller: { type: Boolean },
-    date: { type: Number, required: true }
-})
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  material: { type: String },
+  dimensions: { type: String },
+  color: { type: String },
+  assembly: { type: Boolean },
+  bestseller: { type: Boolean },
+  date: { type: Number, required: true },
+});
 
-const productModel  = mongoose.models.product || mongoose.model("product",productSchema);
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
 
-export default productModel
+export default productModel;
